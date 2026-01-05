3124e0a19491224b9b738e23151848ba2f8a51e5
mister-neisan <misterneisan0@gmail.com>
2026-01-05 20:00:09 +0100

    updates

--- public/inc/haut.inc.php
+++ public/inc/haut.inc.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../static/base.css">
-    <!-- <link rel="stylesheet" href="../static/welcome.css">
+    <link rel="stylesheet" href="../static/style.css">
+    <link rel="stylesheet" href="../static/welcome.css">
     <link rel="stylesheet" href="../static/connexion.css">
     <link rel="stylesheet" href="../static/newsletter.css">
 
@@ -21,7 +21,7 @@
 
 
     <link rel="stylesheet" href="../static/recherche.style">
-    <link rel="stylesheet" href="../static/reservations_details.style"> -->
+    <link rel="stylesheet" href="../static/reservations_details.style">
     <link rel="shortcut icon" href="assets/images/logo.ico" type="image/x-icon">
     <title>LOKISALLE</title>
 </head>
